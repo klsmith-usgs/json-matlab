cefbfa6601e5e987c223661546f8c92500361860
Kelcy Smith <kelcy.smith.ctr@usgs.gov>
2017-03-18 18:12:19 -0500

    let's not all die at once!

--- json_matlab.py
+++ json_matlab.py
@@ -62,36 +62,44 @@ def build_spectral(model, band_names=BAND_NAMES):
 
 
 def worker(args):
-    output_path, h, v, line = args
-    ext, affine = geo_utils.extent_from_hv(h, v)
+    pid = mp.current_process()
 
-    y = ext.y_max - line * 30
-    outfile = os.path.join(output_path, 'record_change{}.mat'.format(line + 1))
-    record = record_template()
+    try:
+        output_path, h, v, line = args
+        print '{}: working line: {}'.format(pid, line)
+        ext, affine = geo_utils.extent_from_hv(h, v)
 
-    sample = 5000 * line
-    for x in xrange(ext.x_min, ext.x_max, 30):
-        sample += 1
-        results = api.request_results(x, y)
+        y = ext.y_max - line * 30
+        outfile = os.path.join(output_path, 'record_change{}.mat'.format(line + 1))
+        record = record_template()
 
-        if results is None:
-            continue
+        sample = 5000 * line
+        for x in xrange(ext.x_min, ext.x_max, 30):
+            sample += 1
+            results = api.request_results(x, y)
 
-        for model in results['change_models']:
-            coefs, rmse, mags = build_spectral(model)
+            if results is None:
+                continue
 
-            record['t_start'].append(pyordinal_to_matordinal(model['start_day']))
-            record['t_end'].append(pyordinal_to_matordinal(model['end_day']))
-            record['t_break'].append(pyordinal_to_matordinal(model['break_day']))
-            record['coefs'].append(coefs)
-            record['rmse'].append(rmse)
-            record['pos'].append(sample)
-            record['change_prob'].append(model['change_probability'])
-            record['num_obs'].append(model['observation_count'])
-            record['category'].append(model['curve_qa'])
-            record['magnitude'].append(mags)
+            for model in results['change_models']:
+                coefs, rmse, mags = build_spectral(model)
 
-    save_record(outfile, record)
+                record['t_start'].append(pyordinal_to_matordinal(model['start_day']))
+                record['t_end'].append(pyordinal_to_matordinal(model['end_day']))
+                record['t_break'].append(pyordinal_to_matordinal(model['break_day']))
+                record['coefs'].append(coefs)
+                record['rmse'].append(rmse)
+                record['pos'].append(sample)
+                record['change_prob'].append(model['change_probability'])
+                record['num_obs'].append(model['observation_count'])
+                record['category'].append(model['curve_qa'])
+                record['magnitude'].append(mags)
+
+        print '{}: writing line: {}'.format(pid, line)
+        save_record(outfile, record)
+
+    except Exception as e:
+        print '{}: hit exception: {}'.format(pid, e)
 
 
 def run(output_path, h, v, cpu_count):
@@ -101,38 +109,6 @@ def run(output_path, h, v, cpu_count):
     pool = mp.Pool(processes=cpu_count)
 
     pool.map(worker, ((output_path, h, v, x) for x in range(5000)))
-
-    # ext, affine = geo_utils.extent_from_hv(h, v)
-    #
-    # line = 0
-    # sample = 0
-    # for y in xrange(ext.y_max, ext.y_min, -30):
-    #     line += 1
-    #     outfile = os.path.join(output_path, 'record_change{}.mat'.format(line))
-    #     record = record_template()
-    #
-    #     for x in xrange(ext.x_min, ext.x_max, 30):
-    #         sample += 1
-    #         results = api.request_results(x, y)
-    #
-    #         if results is None:
-    #             continue
-    #
-    #         for model in results['change_models']:
-    #             coefs, rmse, mags = build_spectral(model)
-    #
-    #             record['t_start'].append(model['start_day'])
-    #             record['t_end'].append(model['end_day'])
-    #             record['t_break'].append(model['break_day'])
-    #             record['coefs'].append(coefs)
-    #             record['rmse'].append(rmse)
-    #             record['pos'].append(sample)
-    #             record['change_prob'].append(model['change_probability'])
-    #             record['num_obs'].append(model['observation_count'])
-    #             record['category'].append(model['curve_qa'])
-    #             record['magnitude'].append(mags)
-    #
-    #     save_mat(outfile, record)
 
 
 if __name__ == '__main__':
