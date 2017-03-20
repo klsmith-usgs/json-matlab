e190495b9be0a075e836f56a1346cab47f35af16
Kelcy Smith <kelcy.smith.ctr@usgs.gov>
2017-03-19 23:18:59 -0500

    numpy data structure for records

--- json_matlab.py
+++ json_matlab.py
@@ -5,6 +5,7 @@ import time
 from functools import wraps
 
 import scipy.io as sio
+import numpy as np
 
 import geo_utils
 import api
@@ -32,16 +33,16 @@ def retry(func, retries=5):
 
 
 def record_template():
-    return {'t_start': [],
-            't_end': [],
-            't_break': [],
-            'coefs': [],
-            'rmse': [],
-            'pos': [],
-            'change_prob': [],
-            'num_obs': [],
-            'category': [],
-            'magnitude': []}
+    return np.zeros(1, dtype=[('t_start', 'i4'),
+                              ('t_end', 'i4'),
+                              ('t_break', 'i4'),
+                              ('coefs', 'f4', (8, 7)),
+                              ('rmse', 'f4', 7),
+                              ('pos', 'i4'),
+                              ('change_prob', 'i4'),
+                              ('num_obs', 'i4'),
+                              ('category', 'i4'),
+                              ('magnitude', 'f4', 7)])
 
 
 def pyordinal_to_matordinal(ord_date):
@@ -53,20 +54,13 @@ def save_record(outfile, record):
 
 
 def build_spectral(model, band_names=BAND_NAMES):
-    coefs = [[0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0]]
-    rmse = []
-    magnitude = []
+    coefs = np.zeros(shape=(8, 7))
+    rmse = np.zeros(shape=(7,))
+    magnitude = np.zeros(shape=(7,))
 
     for i, b in enumerate(band_names):
-        rmse.append(model[b]['rmse'])
-        magnitude.append(model[b]['magnitude'])
+        rmse[i] = model[b]['rmse']
+        magnitude[i] = model[b]['magnitude']
 
         coefs[0][i] = model[b]['intercept']
         for j, val in enumerate(model[b]['coefficients']):
@@ -87,7 +81,7 @@ def worker(args):
 
         y = ext.y_max - line * 30
         outfile = os.path.join(output_path, 'record_change{}.mat'.format(line + 1))
-        record = record_template()
+        records = []
 
         sample = 5000 * line
         for x in xrange(ext.x_min, ext.x_max, 30):
@@ -98,21 +92,24 @@ def worker(args):
                 continue
 
             for model in results['change_models']:
+                record = record_template()
                 coefs, rmse, mags = build_spectral(model)
 
-                record['t_start'].append(pyordinal_to_matordinal(model['start_day']))
-                record['t_end'].append(pyordinal_to_matordinal(model['end_day']))
-                record['t_break'].append(pyordinal_to_matordinal(model['break_day']))
-                record['coefs'].append(coefs)
-                record['rmse'].append(rmse)
-                record['pos'].append(sample)
-                record['change_prob'].append(model['change_probability'])
-                record['num_obs'].append(model['observation_count'])
-                record['category'].append(model['curve_qa'])
-                record['magnitude'].append(mags)
+                record['t_start'] = pyordinal_to_matordinal(model['start_day'])
+                record['t_end'] = pyordinal_to_matordinal(model['end_day'])
+                record['t_break'] = pyordinal_to_matordinal(model['break_day'])
+                record['coefs'] = coefs
+                record['rmse'] = rmse
+                record['pos'] = sample
+                record['change_prob'] = model['change_probability']
+                record['num_obs'] = model['observation_count']
+                record['category'] = model['curve_qa']
+                record['magnitude'] = mags
+
+                records.append(record)
 
         print '{}: writing line: {}, time: {}'.format(pid, line, time.time() - t)
-        save_record(outfile, record)
+        save_record(outfile, np.concatenate(tuple(records)))
 
     except Exception as e:
         print '{}: hit exception: {}'.format(pid, e)
