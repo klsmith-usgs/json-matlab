f47abad0be342068516a91efa8e070c7f7934119
Kelcy Smith <kelcy.smith.ctr@usgs.gov>
2017-03-21 17:13:18 -0500

    better stuff?

--- change_maps.py
+++ change_maps.py
@@ -18,7 +18,7 @@ import change_products
 
 LOGGER = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s')
+formatter = logging.Formatter('%(asctime)s: %(message)s')
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
 LOGGER.setLevel(logging.DEBUG)
@@ -157,36 +157,29 @@ def prod_data_type(product):
         raise ValueError
 
 
-def multi_output(output_dir, output_q, msg_q, kill_count, h, v):
+def multi_output(output_dir, output_q, kill_count, h, v):
     count = 0
     while True:
         if count >= kill_count:
             break
 
-        outdata = output_q.get_nowait()
-        msg = msg_q.get_nowait()
+        outdata = output_q.get()
 
-        if outdata:
-            if outdata == 'kill':
-                count += 1
-                continue
+        if outdata == 'kill':
+            count += 1
+            continue
 
-            # LOGGER.debug('Outputting line: {0}'.format(outdata['y_off']))
-            print 'Outputting line: {}'.format(outdata['y_off'])
-            output_line(outdata, output_dir, h, v)
-
-        if msg:
-            print msg
+        LOGGER.debug('Outputting line: {0}'.format(outdata['y_off']))
+        # print 'Outputting line: {}'.format(outdata['y_off'])
+        output_line(outdata, output_dir, h, v)
 
 
-def multi_worker(input_q, output_q, msg_q):
-    pid = mp.current_process().name
-
+def multi_worker(input_q, output_q, name):
     while True:
         try:
             infile = input_q.get()
 
-            msg_q.put('{}: received - {}'.format(pid, infile))
+            LOGGER.debug('{} - received {}'.format(name, infile))
 
             if infile == 'kill':
                 output_q.put('kill')
@@ -197,9 +190,10 @@ def multi_worker(input_q, output_q, msg_q):
             map_dict = changemap_vals(infile)
             map_dict['y_off'] = int(filename[13:-4])
 
-            msg_q.put('{}: finished - {}'.format(pid, infile))
+            LOGGER.debug('{} - finished {}'.format(name, infile))
             output_q.put(map_dict)
-        except:
+        except Exception as e:
+            LOGGER.debug('{} - exception {}'.format(name, e))
             continue
 
 
@@ -224,7 +218,7 @@ def multi_run(input_dir, output_dir, num_procs, h, v):
         input_q.put('kill')
 
     for _ in range(worker_count):
-        mp.Process(target=multi_worker, args=(input_q, output_q, msg_q)).start()
+        mp.Process(target=multi_worker, args=(input_q, output_q, msg_q, _)).start()
 
     multi_output(output_dir, output_q, msg_q, worker_count, h, v)
 
