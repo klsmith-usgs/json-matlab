c00fc654e30209972ca67f208fa8d5cb9d18ad2e
Kelcy Smith <kelcy.smith.ctr@usgs.gov>
2017-04-03 13:11:16 -0500

    log to stdout

--- logger.py
+++ logger.py
@@ -1,8 +1,11 @@
 import logging
+import sys
 
 log = logging.getLogger()
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('%(asctime)s %(processName)s: %(message)s')
+
 handler.setFormatter(formatter)
+
 log.addHandler(handler)
 log.setLevel(logging.DEBUG)
